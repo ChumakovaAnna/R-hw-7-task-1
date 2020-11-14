f3f5df009b6ad5d84145c0b39275383b9bc068f1
Anna Chumakova <chumakova3008@gmail.com>
2020-11-15 00:08:33 +0500

    Created project

---
+++ src/components/Video.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import DateTime from "./DateTime";
+
+export default function Video(props) {
+  return (
+      <div className="video">
+          <iframe src={props.url} frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+          <DateTime date={props.date} />
+      </div>
+  )
+}
